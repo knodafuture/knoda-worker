79e8c7e7136249a1e8c3fae4c5b0195c01177c39
nick <nrellihan@gmail.com>
2014-05-12 00:12:10 -0500

    image fixes

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,6 @@ module KnodaWorker
     config.paths['db/migrate'] = KnodaCore::Engine.paths['db/migrate'].existent
     config.twitter_key = "14fSb3CT7EEQkoryO8RNx7BrG"
     config.twitter_secret = "6Z5OGzxLL9NqVEpAbLs9FFd2PyLm6pd7j5r98IZr5e0HRr73bo"
+    config.asset_host = ENV['ASSET_HOST']
   end
 end
