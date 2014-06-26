b22459a00cb7234f4b08d999c1bbe44d1f3ca1bd
Adam England <adam.n.england@gmail.com>
2014-06-26 06:50:02 -0500

    Configuring scheduled workers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.cache_classes = true
-  config.eager_load = false
+  config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
